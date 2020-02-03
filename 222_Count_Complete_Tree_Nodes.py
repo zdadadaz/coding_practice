cb115a9d7caeeb2750629e45b0ae381a1658b6ac
zdadadaz <zdadadaz5566@gmail.com>
2020-02-03 17:45:05 +1000

    add 222 whileloop and complete tree condition

--- 222_Count_Complete_Tree_Nodes.py
+++ 222_Count_Complete_Tree_Nodes.py
@@ -1,3 +1,10 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def countNodes(self, root: TreeNode) -> int:
         if root == None:
@@ -5,8 +12,10 @@ class Solution:
         if root.left == None and root.right == None:
             return 1
         count = [0]
-        self.dfs(root, count)
-        return count[0] 
+        # self.dfs(root, count)
+        # return count[0]
+        # return self.dfs_m(root)
+        return self.complete_tree_check(root)
     
     def dfs(self, node, count):
         if node.left != None:
@@ -14,6 +23,41 @@ class Solution:
         if node.right != None:
             self.dfs(node.right, count)
         count[0] += 1
+    
+#   while loop
+    def dfs_m(self, node):
+        queue = []
+        queue.append(node)
+        count = 0
+        while len(queue) != 0:
+            cur = queue.pop()
+            count += 1
+            if cur.left !=None:
+                queue.append(cur.left)
+            if cur.right !=None:
+                queue.append(cur.right)
+        return count
+    
+    def complete_tree_check(self, node):
+        if node == None:
+            return 0
+        left_height = 1
+        tmp_node = node
+        while tmp_node.left:
+            tmp_node = tmp_node.left
+            left_height += 1
+        right_height = 1
+        tmp_node = node
+        while tmp_node.right:
+            tmp_node = tmp_node.right
+            right_height += 1
+            
+        if left_height == right_height:
+            return 2**left_height -1
+        return (1+ self.complete_tree_check(node.left) + self.complete_tree_check(node.right))
+        
+            
+        
 
 sol = Solution()
 
